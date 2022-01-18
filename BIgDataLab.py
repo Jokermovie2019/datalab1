f2587c28f03f6ae1bcb00aa2c1be5c0ac071b8da
Jokermovie2019 <sjpitman22@students.bmchs.org>
2022-01-18 08:38:55 -0600

    finished big data

--- BIgDataLab.py
+++ BIgDataLab.py
@@ -1,6 +1,6 @@
 import csv
 
-with open('Norman2016.csv', newline='') as csvfile:
+with open('BigData2016.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     
     class Station():
@@ -9,23 +9,38 @@ with open('Norman2016.csv', newline='') as csvfile:
             self.maxTemp = maxTemp
             self.minTemp = minTemp
 
-        def findMax(self, row):
+        def findPeak(self, row):
             if row['STID'] == self.stid:
                 if float(row['TMAX']) != -996.00:
                     if self.maxTemp < float(row['TMAX']):
                         self.maxTemp = float(row['TMAX'])
-
-        def findMin(self, row):
-            if row['STID'] == self.stid:
-                if float(row['TMAX']) != -996.00:
                     if self.minTemp > float(row['TMAX']):
                         self.minTemp = float(row['TMAX'])
-
+        
+        def disp(self):
+            print(f"{self.stid} max: {self.maxTemp} min: {self.minTemp}")
+        
     ard2 = Station("ARD2", 0, 150)
     beav = Station("BEAV", 0, 150)
+    bois = Station("BOIS", 0, 150)
+    cent = Station("CENT", 0, 150)
+    nrmn = Station("NRMN", 0, 150)
+    stil = Station("STIL", 0, 150)
+    tish = Station("TISH", 0, 150)
+    tuln = Station("TULN", 0, 150)
+    wood = Station("WOOD", 0, 150)
 
+    stations = [ard2, beav, bois, cent, nrmn, stil, tish, tuln, wood]
+    
     for row in reader:
-        ard2.findMax(row)
-        ard2.findMin(row)
+        for station in stations:
+            station.findPeak(row)
+            
+        
+    for station in stations:
+        station.disp()
+        
 
-print(ard2.maxTemp)
+    
+
+
